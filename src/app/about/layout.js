0d6a80c16266b11a60eb084875c9ee094b935981
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-12-24 16:24:36 +0530

    conditional layout

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,12 +1,23 @@
+'use client'
 import React from 'react'
-
+ import { usePathname } from 'next/navigation'
 function commonabout({children}) {
+  const path = usePathname();
+  console.log(path)
   return (
+    <>
+    
     <div>
+    { path !== '/about/aboutstudent' ?(
         <h1>About Common Page</h1>
+      )
+      : null }
         <br /><br /><br />
        {children}
     </div>
+    
+ 
+  </>
   )
 }
 
